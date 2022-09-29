3eb7362155feeeca2d2ed3c47c11eb3074e3ad50
Braidan <legohead259@gmail.com>
2022-09-28 23:32:53 -0400

    Added LoRa libary

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 #include <LoRa.h>
+#include <SPI.h>
 
 // LoRa Instantiation
 #define RFM_CS_PIN 38
 #define RFM_RST_PIN 0
 #define RFM_IRQ_PIN 33
+
+SPIClass LoRaSPI(FSPI);
 
 void setup() {
 	Serial.begin(115200);
@@ -13,6 +16,8 @@ void setup() {
 	// Initialize LoRa Radio
 	Serial.print("Initializing radio...");
     LoRa.setPins(RFM_CS_PIN, RFM_RST_PIN, RFM_IRQ_PIN);
+    LoRaSPI.begin(37, 36, 35, 38);
+    LoRa.setSPI(LoRaSPI);
     
     if (!LoRa.begin(915E6)) {
         Serial.println("Starting LoRa failed!");
@@ -22,12 +27,26 @@ void setup() {
     Serial.println("done!");
 
 	// Initialize filesystem
-	#define SDCARD_DEBUG
-	if (!initSDCard()) { // Initialize filesystem and check if good
-        // while(true) blinkCode(CARD_MOUNT_ERROR_CODE); // Block further code execution
-    }
+	// #define SDCARD_DEBUG
+	// if (!initSDCard()) { // Initialize filesystem and check if good
+    //     // while(true) blinkCode(CARD_MOUNT_ERROR_CODE); // Block further code execution
+    // }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // try to parse packet
+    int packetSize = LoRa.parsePacket();
+    if (packetSize) {
+        // received a packet
+        Serial.print("Received packet '");
+
+        // read packet
+        while (LoRa.available()) {
+        Serial.print((char)LoRa.read(), HEX);
+        }
+
+        // print RSSI of packet
+        Serial.print("' with RSSI ");
+        Serial.println(LoRa.packetRssi());
+    }
 }
